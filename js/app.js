3eff699530f144d897776aae6543393e24260944
mrabenda92 <exo92mr@gmail.com>
2019-07-03 00:08:50 +0200

    js lock

--- js/app.js
+++ js/app.js
@@ -1,40 +1,40 @@
-const smallOrg = document.querySelector(".small_orgo");
-const mediumOrg = document.querySelector(".medium_orgo");
-const bigOrg = document.querySelector(".big_orgo");
-const pictureOrg = document.querySelector(".orgo_pict");
+// const smallOrg = document.querySelector(".small_orgo");
+// const mediumOrg = document.querySelector(".medium_orgo");
+// const bigOrg = document.querySelector(".big_orgo");
+// const pictureOrg = document.querySelector(".orgo_pict");
 
-smallOrg.addEventListener('mouseover', function() {
-    pictureOrg.style.backgroundImage = "url(../orgonity/IMG_20181021_130812.jpg)";
-    pictureOrg.style.backgroundPosition = "center center";
-    console.log('test1');
-})
+// smallOrg.addEventListener('mouseover', function() {
+//     pictureOrg.style.backgroundImage = "url(../orgonity/IMG_20181021_130812.jpg)";
+//     pictureOrg.style.backgroundPosition = "center center";
+//     console.log('test1');
+// })
 
-smallOrg.addEventListener('mouseleave', function() {
-    pictureOrg.style.backgroundImage = "url(../orgonity/IMG_20181011_193101.jpg)";
-    pictureOrg.style.backgroundPosition = "center center";
-    console.log('test1');
-})
+// smallOrg.addEventListener('mouseleave', function() {
+//     pictureOrg.style.backgroundImage = "url(../orgonity/IMG_20181011_193101.jpg)";
+//     pictureOrg.style.backgroundPosition = "center center";
+//     console.log('test1');
+// })
 
-mediumOrg.addEventListener('mouseover', function() {
-    pictureOrg.style.backgroundImage = "url(../orgonity/IMG_20181021_130119.jpg)";
-    pictureOrg.style.backgroundPosition = "center bottom";
-    console.log('test2');
-})
+// mediumOrg.addEventListener('mouseover', function() {
+//     pictureOrg.style.backgroundImage = "url(../orgonity/IMG_20181021_130119.jpg)";
+//     pictureOrg.style.backgroundPosition = "center bottom";
+//     console.log('test2');
+// })
 
-mediumOrg.addEventListener('mouseleave', function() {
-    pictureOrg.style.backgroundImage = "url(../orgonity/IMG_20181011_193101.jpg)";
-    pictureOrg.style.backgroundPosition = "center center";
-    console.log('test1');
-})
+// mediumOrg.addEventListener('mouseleave', function() {
+//     pictureOrg.style.backgroundImage = "url(../orgonity/IMG_20181011_193101.jpg)";
+//     pictureOrg.style.backgroundPosition = "center center";
+//     console.log('test1');
+// })
 
-bigOrg.addEventListener('mouseover', function() {
-    pictureOrg.style.backgroundImage = "url(../orgonity/IMG_20181021_130017.jpg)";
-    pictureOrg.style.backgroundPosition = "center top";
-    console.log('test3');
-})
+// bigOrg.addEventListener('mouseover', function() {
+//     pictureOrg.style.backgroundImage = "url(../orgonity/IMG_20181021_130017.jpg)";
+//     pictureOrg.style.backgroundPosition = "center top";
+//     console.log('test3');
+// })
 
-bigOrg.addEventListener('mouseleave', function() {
-    pictureOrg.style.backgroundImage = "url(../orgonity/IMG_20181011_193101.jpg)";
-    pictureOrg.style.backgroundPosition = "center center";
-    console.log('test1');
-})
+// bigOrg.addEventListener('mouseleave', function() {
+//     pictureOrg.style.backgroundImage = "url(../orgonity/IMG_20181011_193101.jpg)";
+//     pictureOrg.style.backgroundPosition = "center center";
+//     console.log('test1');
+// })
